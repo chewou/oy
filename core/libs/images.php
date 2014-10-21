8687a74f68c536158c74ed98e98863f42b79c253
omita <2069410891@qq.com>
2014-10-21 22:49:56 +0800

    images class
    add a class about iamges which has many methods,such as waterMark
    verifyImage and so on

---
+++ core/libs/images.php
@@ -0,0 +1,15 @@
+<?php 
+// 有关图像处理方面的对象
+class images{
+	// 验证码函数
+	public function verifyImage(){
+		#code
+	}
+	// 生成缩略图
+	public function thumb(){}
+	// 添加图片水印
+	public function waterMaek(){}
+}
+
+
+ ?>
